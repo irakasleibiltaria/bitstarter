11c0324a8806cfea966f4c55a2513b80c1423a9b
irakasleibiltaria <irakasleibiltaria@gmail.com>
2013-07-19 19:08:04 +0200

    web.js 8080 port and add .gitignore

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res){
       res.sendfile(__dirname + '/index.html');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
