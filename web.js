eaf292f04c45e4b81cf5e0b92b3c6fbd710c17f8
irakasleibiltaria <irakasleibiltaria@gmail.com>
2013-07-20 22:48:56 +0200

    update

--- web.js
+++ web.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res){
       res.sendfile(__dirname + '/index.html');
 });
 
+app.get('/css/:id', function(req, res) {
+  res.sendfile(__dirname + '/css/' + req.params.id);
+});
+
+app.get('/js/:id', function(req, res) {
+  res.sendfile(__dirname + '/js/' + req.params.id);
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
